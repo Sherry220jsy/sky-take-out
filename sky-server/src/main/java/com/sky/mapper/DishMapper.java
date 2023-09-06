f389dfcfc71c5430c414df09b65011a3cbc34303
Sherry <2838916231@qq.com>
2023-09-06 17:21:53 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
+
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish) ;
 
     /**
      * 根据分类id查询菜品数量
